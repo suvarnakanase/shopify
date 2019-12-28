3ed069c4f41d4b17c5142a7f0d2d876731ede741
suvarnakanase <kanasesuvarna@gmail.com>
2019-12-28 21:43:08 +0530

    guard and auth services

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService , private rou: Router) { }
 
   ngOnInit() {
+    this.auth.removeval();
+    this.rou.navigate(['/login']);
   }
 
 }
